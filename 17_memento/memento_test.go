c3c170b8b294e5e3d52abd5aafa00443aab87841
sunqing <1033915839@qq.com>
2023-09-27 20:13:34 +0800

    memento test

--- 17_memento/memento_test.go
+++ 17_memento/memento_test.go
@@ -12,11 +12,21 @@ func ExampleGame() {
 	game.Play(-2, -3)
 	game.Status()
 
-	game.Load(progress)
+	//游戏过程中保存一次
+	progress1 := game.Save()
 	game.Status()
 
+	//查看进度1的状态
+	game.Load(progress1)
+	game.Status()
+
+	//查看初始状态
+	game.Load(progress)
+	game.Status()
 	// Output:
 	// Current HP:10, MP:10
+	// Current HP:7, MP:8
+	// Current HP:7, MP:8
 	// Current HP:7, MP:8
 	// Current HP:10, MP:10
 }
